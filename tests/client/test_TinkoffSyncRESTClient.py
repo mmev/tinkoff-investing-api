591382c35fbc3f57ed627c5462aa370207fc4167
Alexey Voronin <mmev.free@gmail.com>
2021-02-01 01:27:12 +0300

    feat(SyncRestClient): add is_sandbox property

--- tests/client/test_TinkoffSyncRESTClient.py
+++ tests/client/test_TinkoffSyncRESTClient.py
@@ -29,3 +29,30 @@ def test_client_property_token_cannot_be_changed():
     client = TinkoffSyncRESTClient(token='<TOKEN>')
     with pytest.raises(AttributeError):
         client.token = '<NEW_TOKEN>'
+
+
+def test_client_have_property_is_sandbox():
+    client = TinkoffSyncRESTClient(token='<TOKEN>')
+    assert hasattr(client, 'is_sandbox')
+
+
+def test_client_property_is_sandbox_return_bool():
+    client = TinkoffSyncRESTClient(token='<TOKEN>')
+    assert isinstance(client.is_sandbox, bool)
+
+
+def test_client_property_is_sandbox_cannot_be_changed():
+    client = TinkoffSyncRESTClient(token='<TOKEN>')
+    with pytest.raises(AttributeError):
+        client.is_sandbox = False
+
+
+def test_client_property_is_sandbox_default_false():
+    client = TinkoffSyncRESTClient(token='<TOKEN>')
+    assert client.is_sandbox is False
+
+
+def test_client_property_is_sandbox_can_be_set_in_constructor():
+    set_value = True
+    client = TinkoffSyncRESTClient(token='<TOKEN>', is_sandbox=set_value)
+    assert client.is_sandbox == set_value
--- tinkoff_investing_api/client/TinkoffSyncRESTClient.py
+++ tinkoff_investing_api/client/TinkoffSyncRESTClient.py
@@ -2,9 +2,14 @@
 
 class TinkoffSyncRESTClient:
 
-    def __init__(self, token: str) -> None:
+    def __init__(self, token: str, is_sandbox: bool = False) -> None:
         self.__token = token
+        self.__is_sandbox = is_sandbox
 
     @property
     def token(self) -> str:
         return self.__token
+
+    @property
+    def is_sandbox(self) -> bool:
+        return self.__is_sandbox
